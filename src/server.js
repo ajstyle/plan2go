e42c2ebe2b7aef3e927a80e924d5b303493dce30
amitjain <omertabib3004@gmail.com>
2019-08-23 13:42:55 +0530

    make it better

--- src/server.js
+++ src/server.js
@@ -7,8 +7,9 @@ app.use(express.static(__dirname + '/dist/plan2go'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/plan2go/index.html'));
+  res.sendFile(path.join (__dirname + '/dist/plan2go/index.html'));
 });
+
 
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
